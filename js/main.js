c1b113d560eb33f0ab0f2779e3de5f03661de25b
nkosyrev <shutnik1986@gmail.com>
2022-09-08 12:18:45 +0900

    fix: fix main.js

--- js/main.js
+++ js/main.js
@@ -26,8 +26,8 @@ const closeMenu = (event) => {
   menu.classList.remove("is-open"); // убирает класс is-open
   mMenuToggle.classList.remove("close-menu");
   document.body.style.overflow = ""; // возвращает прокрутку сайта под меню
-  lightModeOff();
-}
+  this.scrollY < 1 ? lightModeOff() : lightModeOn();
+};
 
 window.addEventListener('scroll', () => {
   this.scrollY > 1 ? lightModeOn() : lightModeOff();
