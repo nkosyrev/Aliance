88d9e72558e02ba3b071077a268d8082eb6d668e
nkosyrev <shutnik1986@gmail.com>
2022-10-07 15:02:51 +0900

    add: founder.webp

--- template-parts/founder-block.php
+++ template-parts/founder-block.php
@@ -1,21 +1,25 @@
 <section class="section founder">
-      <img src="img/founder.jpg" alt="founder" class="founder-photo">
-      <div class="container">
-        <div class="founder-content-wrapper">
-          <div class="founder-content">
-            <div class="seporator"></div>
-            <h2 class="section-title">Отношение к делу<br> и к клиентам</h2>
-            <p class="founder-text">
+  <picture class="founder-photo">
+    <source type="image/webp" srcset="img/founder.webp">
+    <source type="image/jpeg" srcset="img/founder.jpg">
+    <img src="img/founder.jpg"  alt="founder">
+  </picture>
+  <div class="container">
+    <div class="founder-content-wrapper">
+      <div class="founder-content">
+        <div class="seporator"></div>
+        <h2 class="section-title">Отношение к делу<br> и к клиентам</h2>
+        <p class="founder-text">
               Кстати, интерактивные прототипы описаны максимально подробно. Повседневная практика показывает, что укрепление и развитие внутренней структуры говорит о возможностях соответствующих условий активизации. Внезапно, независимые государства, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут подвергнуты целой серии независимых исследований. С учётом сложившейся международной обстановки, синтетическое тестирование выявляет срочную потребность системы массового участия.
-            </p>
-            <p class="founder-text">
+        </p>
+        <p class="founder-text">
               А ещё действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены сами себе. Не следует, однако, забывать, что выбранный нами инновационный путь в значительной степени обусловливает важность дальнейших направлений развития.
-            </p>
-            <a href="" class="button-link"><span class="button-link-big">Подробнее о компании</span> <span class="button-link-small">О нашей миссии</span></a>
-          </div>
-        </div>
-        <!-- /.founder-content-wrapper -->
+        </p>
+        <a href="" class="button-link"><span class="button-link-big">Подробнее о компании</span> <span class="button-link-small">О нашей миссии</span></a>
       </div>
-      <!-- /.container -->
-    </section>
+    </div>
+    <!-- /.founder-content-wrapper -->
+  </div>
+  <!-- /.container -->
+</section>
     <!-- /.section founder -->
